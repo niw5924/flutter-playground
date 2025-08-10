c30df985fe03c3322340987eebcc615a3e70e8b8
niw5924 <niw5924@naver.com>
2025-08-10 23:45:28 +0900

    feat: 인터렉션 시작시 BoxFit.contain으로 변환

--- lib/page_view/page_view_screen.dart
+++ lib/page_view/page_view_screen.dart
@@ -8,20 +8,20 @@ class PageViewScreen extends StatefulWidget {
 }
 
 class _PageViewScreenState extends State<PageViewScreen> {
-  final Set<int> _pointers = {}; // 현재 화면에 닿아있는 포인터 id들
+  final Set<int> _pointers = {};
   bool _lockScroll = false;
 
   void _handlePointerDown(PointerDownEvent e) {
     _pointers.add(e.pointer);
     if (_pointers.length >= 2 && !_lockScroll) {
-      setState(() => _lockScroll = true); // 두 손가락 이상: 스크롤 잠금
+      setState(() => _lockScroll = true);
     }
   }
 
   void _handlePointerUpOrCancel(PointerEvent e) {
     _pointers.remove(e.pointer);
     if (_pointers.length < 2 && _lockScroll) {
-      setState(() => _lockScroll = false); // 한 손가락 이하: 스크롤 해제
+      setState(() => _lockScroll = false);
     }
   }
 
@@ -38,16 +38,34 @@ class _PageViewScreenState extends State<PageViewScreen> {
                 ? const NeverScrollableScrollPhysics()
                 : const PageScrollPhysics(),
         itemBuilder: (context, index) {
-          return InteractiveViewer(
-            minScale: 1.0,
-            maxScale: 3.0,
-            child: Image.asset(
-              'assets/image/pv${index + 1}.png',
-              fit: BoxFit.cover,
-            ),
-          );
+          return _ZoomableImage(path: 'assets/image/pv${index + 1}.png');
         },
       ),
+    );
+  }
+}
+
+class _ZoomableImage extends StatefulWidget {
+  final String path;
+
+  const _ZoomableImage({required this.path});
+
+  @override
+  State<_ZoomableImage> createState() => _ZoomableImageState();
+}
+
+class _ZoomableImageState extends State<_ZoomableImage> {
+  BoxFit _fit = BoxFit.cover;
+
+  @override
+  Widget build(BuildContext context) {
+    return InteractiveViewer(
+      minScale: 1.0,
+      maxScale: 3.0,
+      onInteractionStart: (_) {
+        setState(() => _fit = BoxFit.contain);
+      },
+      child: Image.asset(widget.path, fit: _fit),
     );
   }
 }

eddd4901ff0c70f8d5034b9e72275bb071aca14b
niw0906 <niw0906@naver.com>
2025-08-25 20:56:17 +0900

    chore: AirBridge 설정(Android)

---
+++ android/app/src/main/kotlin/com/example/flutter_niw/MainApplication.kt
@@ -0,0 +1,9 @@
+import co.ab180.airbridge.flutter.AirbridgeFlutter
+import android.app.Application
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AirbridgeFlutter.initializeSDK(this, "flutterniwdev", "80a37e6edb594b18a107e3d4a32abe2f")
+    }
+}

fdaf5e114e03e933a1a0161fa02577d5c82f39d8
niw0906 <niw0906@naver.com>
2025-08-26 01:57:05 +0900

    feat: main.dart AirBridge 딥링크 콜백 선언

--- android/app/src/main/kotlin/com/example/flutter_niw/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_niw/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.example.flutter_niw
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import co.ab180.airbridge.flutter.AirbridgeFlutter
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        AirbridgeFlutter.trackDeeplink(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
+}

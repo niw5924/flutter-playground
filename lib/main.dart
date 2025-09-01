a3665c158adc5ed8d00797be876c9ee4a2265565
niw0906 <niw0906@naver.com>
2025-09-02 00:15:58 +0900

    chore: 딥링크 로그 출력 - runApp이 실행된후에 딥링크실행(앱이 꺼진상태에서) - 정상

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,15 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   Airbridge.setOnDeeplinkReceived((deeplink) {
-    debugPrint('Airbridge deeplink: $deeplink');
+    print('Airbridge deeplink: $deeplink');
     final uri = Uri.tryParse(deeplink);
     if (uri == null) return;
 
     final uriStr = uri.toString();
     final code = uri.queryParameters['code'];
 
-    debugPrint("남인우 바보$uriStr");
-    debugPrint("남인우 메롱$code");
+    print("남인우 바보$uriStr");
+    print("남인우 메롱$code");
 
     // 0.5초 대기 후 goRouter 사용
     Future.delayed(const Duration(milliseconds: 500), () {
@@ -24,6 +24,7 @@ void main() {
     print("통과함통과함통과함통과함통과함");
   });
 
+  print("MyApp이 실행됨");
   runApp(const MyApp());
 }
 

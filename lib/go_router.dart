d6b0890386345e6f2733d4a899cc540068df9803
niw0906 <niw0906@naver.com>
2025-09-01 23:32:54 +0900

    feat: 에어브릿지 QR 화면 go_router로 변환

--- lib/go_router.dart
+++ lib/go_router.dart
@@ -1,16 +1,21 @@
+import 'package:flutter_niw/airbridge_qr/airbridge_qr_screen.dart';
 import 'package:go_router/go_router.dart';
-
-import 'popup_sequence/popup_sequence_screen.dart';
 import 'popup_sequence/screen_a.dart';
 import 'popup_sequence/screen_b.dart';
+import 'airbridge_qr/code_screen.dart';
 
 final goRouter = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const PopupSequenceScreen(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const AirbridgeQrScreen()),
     GoRoute(path: '/a', builder: (context, state) => const ScreenA()),
     GoRoute(path: '/b', builder: (context, state) => const ScreenB()),
+    GoRoute(
+      path: '/code',
+      builder: (context, state) {
+        final code = state.uri.queryParameters['code']!;
+        final fullUri = state.uri.toString();
+        return CodeScreen(code: code, fullUri: fullUri);
+      },
+    ),
   ],
 );
